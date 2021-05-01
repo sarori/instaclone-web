d43ececef9e8a04ffffdca631060e94f9152b824
sarori <salo3678@gmail.com>
2021-05-01 22:37:25 +0200

    Auth POC

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,3 +1,9 @@
-const Login = () => <h1>Login</h1>
-
+function Login({ setIsLoggedIn }) {
+	return (
+		<div>
+			<h1>Login</h1>
+			<button onClick={() => setIsLoggedIn(true)}>Log in now!</button>
+		</div>
+	)
+}
 export default Login
