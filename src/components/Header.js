8e78f3d6ad78d87a8045f7528d72df433dc77eb8
sarori <salo3678@gmail.com>
2021-05-10 22:55:20 +0200

    Header part Three

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
+import { useReactiveVar } from "@apollo/client"
 import { faInstagram } from "@fortawesome/free-brands-svg-icons"
 import { faCompass, faUser } from "@fortawesome/free-regular-svg-icons"
 import { faHome } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
+import { isLoggedInVar } from "../apollo"
+import useUser from "../hooks/useUser"
+import routes from "../routes"
 
 const SHeader = styled.header`
 	width: 100%;
@@ -28,7 +33,17 @@ const Icon = styled.span`
 	margin-left: 15px;
 `
 
+const Button = styled.span`
+	background-color: ${(props) => props.theme.accent};
+	border-radius: 4px;
+	padding: 3px 15px;
+	color: white;
+	font-weight: 600;
+`
+
 function Header() {
+	const isLoggedIn = useReactiveVar(isLoggedInVar)
+	const loggedInUser = useUser()
 	return (
 		<SHeader>
 			<Wrapper>
@@ -36,15 +51,23 @@ function Header() {
 					<FontAwesomeIcon icon={faInstagram} size="2x" />
 				</Column>
 				<Column>
-					<Icon>
-						<FontAwesomeIcon icon={faHome} size="1g" />
-					</Icon>
-					<Icon>
-						<FontAwesomeIcon icon={faCompass} size="1g" />
-					</Icon>
-					<Icon>
-						<FontAwesomeIcon icon={faUser} size="1g" />
-					</Icon>
+					{isLoggedIn ? (
+						<>
+							<Icon>
+								<FontAwesomeIcon icon={faHome} size="lg" />
+							</Icon>
+							<Icon>
+								<FontAwesomeIcon icon={faCompass} size="lg" />
+							</Icon>
+							<Icon>
+								<FontAwesomeIcon icon={faUser} size="lg" />
+							</Icon>
+						</>
+					) : (
+						<Link href={routes.home}>
+							<Button>Login</Button>
+						</Link>
+					)}
 				</Column>
 			</Wrapper>
 		</SHeader>
